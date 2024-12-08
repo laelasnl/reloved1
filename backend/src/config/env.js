4fd220d0b994a563d754c1cb8dcda1e3b8990be6
Siti Nurlaela <ns793742@gmail.com>
2024-12-08 20:44:36 +0700

    push

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3001,
   mongodbUri: process.env.MONGODB_URI || "mongodb://localhost:27017",
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
